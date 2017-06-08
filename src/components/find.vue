2f6b1efafd74743f1179bf496c649cb632bbed77
周宝 <baobaozhou199606@gmail.com>
2017-06-08 17:11:37 +0800

    what: 编辑babelrc文件，为按需引入组件做准备。同时移除element-ui的配置
    Why:
    
    *
    
    How:
    
    * edit babelrc  file

--- src/components/find.vue
+++ src/components/find.vue
@@ -1,46 +1,39 @@
 <template>
-  <div class = "confirmation">
-    
+  <div class="confirmation">
+
   </div>
 </template>
 
 <script>
-import { Cell } from 'mint-ui';
+  import { Cell } from 'mint-ui';
 
-
-export default {
-  name: 'Confirmation',
-  data () {
-    return {
-    }
-  },
-  methods:{
-
-    }
-  },
-  components:{
-
+  export default {
+    name: 'Confirmation',
+    data () {
+      return {}
+    },
+    methods: {}
+    ,
+    components: {}
   }
-}
 
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
-  a{
-    text-decoration:none;
+  a {
+    text-decoration: none;
   }
 
-  ul, li{
-    list-style:none;
+  ul, li {
+    list-style: none;
   }
 
-  .find{
-    height:100%;
-    width:100%;
+  .find {
+    height: 100%;
+    width: 100%;
   }
-
 
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import App from './App'
 import router from './router'
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
-
-
 Vue.config.productionTip = false
 Vue.use(Mint)
 
